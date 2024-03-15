41f481f05573739802d70e61566e45759c509b55
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-03-15 19:04:15 +0330

    tailwind configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 
 const App : React.FC = () => {
   return (
-    <>
-      <h1 className="text-center">Initialize with tailwind and vite</h1>
-    </>
+    <div id='app' className="relative bg-darkColor min-h-screen">
+      {/* background shape */}
+      <div className="bg-yellowColor "></div>
+      <h1 className="text-center text-white">Initialize with tailwind and vite</h1>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./src/**/*.{ts,tsx}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellowColor: "#ffb400",
+        darkColor: "#111111",
+      },
+    },
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      xxl: "1400px",
+    },
   },
   plugins: [],
 };
