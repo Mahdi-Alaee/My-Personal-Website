f3dc25f585b75d3d293fd77fa8a9473637030608
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-03-15 20:14:38 +0330

    background shape

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ const App : React.FC = () => {
   return (
     <div id='app' className="relative bg-darkColor min-h-screen">
       {/* background shape */}
-      <div className="bg-yellowColor "></div>
-      <h1 className="text-center text-white">Initialize with tailwind and vite</h1>
+      <div className="absolute bg-yellowColor h-screen w-1/4 skew-x-12 -left-36 hidden lg:block"></div>
+
     </div>
   )
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,8 +1,8 @@
 const Menu: React.FC = () => {
     return (
-        <>
-
-        </>
+        <ul>
+            
+        </ul>
     )
 }
 
