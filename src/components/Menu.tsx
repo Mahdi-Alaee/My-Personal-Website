6001957ecf448ead1746bdaf4571e9cbea342dbf
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-03-16 16:56:07 +0330

    fix menu bug

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -17,7 +17,7 @@ const Menu: React.FC<MenuProps> = ({ isMenuOpen }) => {
       className={`absolute top-0 right-0 left-0 bottom-0 pt-16 px-8 bg-darkBrown text-white 
      text-lg duration-500 sm:text-2xl ${
        isMenuOpen ? "" : "-left-96 right-[100vw]"
-     } lg:bg-transparent lg:right-1 lg:w-auto lg:h-auto lg:text-lg lg:top-24`}
+     } lg:bg-transparent lg:right-1 lg:w-60 lg:h-auto lg:text-lg lg:top-48 lg:left-auto lg:flex lg:flex-col lg:items-end`}
     >
       <Link
         className="group flex items-center gap-x-4 py-4 border-b duration-200 border-lightBrown last:border-none 
@@ -27,7 +27,7 @@ const Menu: React.FC<MenuProps> = ({ isMenuOpen }) => {
         <div className="lg:bg-lightBrown lg:p-4 lg:rounded-full duration-200 lg:group-hover:bg-yellowColor">
           <FaHome />
         </div>
-        <span className="duration-200 lg:opacity-0 lg:group-hover:opacity-100 lg:pl-6 lg:text-base">HOME</span>
+        <span className="duration-200 lg:opacity-0 lg:group-hover:opacity-100 lg:pl-6 lg:text-sm">HOME</span>
       </Link>
       <Link
         className="group flex items-center gap-x-4 py-4 border-b duration-200 border-lightBrown last:border-none 
@@ -37,7 +37,7 @@ const Menu: React.FC<MenuProps> = ({ isMenuOpen }) => {
         <div className="lg:bg-lightBrown lg:p-4 lg:rounded-full duration-200 lg:group-hover:bg-yellowColor">
           <FaUser />
         </div>
-        <span className="duration-200 lg:opacity-0 lg:group-hover:opacity-100 lg:pl-6 lg:text-base">ABOUT</span>
+        <span className="duration-200 lg:opacity-0 lg:group-hover:opacity-100 lg:pl-6 lg:text-sm">ABOUT</span>
       </Link>
       <Link
         className="group flex items-center gap-x-4 py-4 border-b duration-200 border-lightBrown last:border-none 
@@ -47,7 +47,7 @@ const Menu: React.FC<MenuProps> = ({ isMenuOpen }) => {
         <div className="lg:bg-lightBrown lg:p-4 lg:rounded-full duration-200 lg:group-hover:bg-yellowColor">
           <FaBriefcase />
         </div>
-        <span className="duration-200 lg:opacity-0 lg:group-hover:opacity-100 lg:pl-6 lg:text-base">PORTFOLIO</span>
+        <span className="duration-200 lg:opacity-0 lg:group-hover:opacity-100 lg:pl-6 lg:text-sm">PORTFOLIO</span>
       </Link>
       <Link
         className="group flex items-center gap-x-4 py-4 border-b duration-200 border-lightBrown last:border-none 
@@ -57,7 +57,7 @@ const Menu: React.FC<MenuProps> = ({ isMenuOpen }) => {
         <div className="lg:bg-lightBrown lg:p-4 lg:rounded-full duration-200 lg:group-hover:bg-yellowColor">
           <FaEnvelopeOpen />
         </div>
-        <span className="duration-200 lg:opacity-0 lg:group-hover:opacity-100 lg:pl-6 lg:text-base">CONTACT</span>
+        <span className="duration-200 lg:opacity-0 lg:group-hover:opacity-100 lg:pl-6 lg:text-sm">CONTACT</span>
       </Link>
       <Link
         className="group flex items-center gap-x-4 py-4 border-b duration-200 border-lightBrown last:border-none 
@@ -67,7 +67,7 @@ const Menu: React.FC<MenuProps> = ({ isMenuOpen }) => {
         <div className="lg:bg-lightBrown lg:p-4 lg:rounded-full duration-200 lg:group-hover:bg-yellowColor">
           <FaComments />
         </div>
-        <span className="duration-200 lg:opacity-0 lg:group-hover:opacity-100 lg:pl-6 lg:text-base">BLOG</span>
+        <span className="duration-200 lg:opacity-0 lg:group-hover:opacity-100 lg:pl-6 lg:text-sm">BLOG</span>
       </Link>
     </ul>
   );
