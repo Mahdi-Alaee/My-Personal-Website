b468ea61626e3914db34858ba4cd4bdb6e5ca2a3
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-03-18 16:37:32 +0330

    coding Index page html structure

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,35 @@
+import { FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 const Index: React.FC = () => {
   return (
-    <>
-      <h1>index page</h1>
-    </>
+    <div>
+      {/* profile image */}
+      <img
+        src="https://tunis-next.netlify.app/assets/img/dark.jpg"
+        alt="mahdi alaee | مهدی علایی"
+      />
+      {/* page content */}
+      <div>
+        {/* introduction */}
+        <h1>I'M MAHDI ALAEE.</h1>
+        {/* job */}
+        <p>FRONT-END DEVELOPER</p>
+        {/* description */}
+        <p>
+          I'm a Tunisian based web designer & front‑end developer focused on
+          crafting clean & user‑friendly experiences, I am passionate about
+          building excellent software that improves the lives of those around
+          me.
+        </p>
+        <Link to="/about">
+          <span>MORE ABOUT ME</span>
+          <span>
+            <FaArrowRight />
+          </span>
+        </Link>
+      </div>
+    </div>
   );
 };
 
