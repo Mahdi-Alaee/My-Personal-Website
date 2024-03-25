2a24bcf121042404320826954e1d12a914ba9aa7
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-03-25 17:11:17 +0330

    create EBox component

---
+++ src/components/EBox.tsx
@@ -0,0 +1,27 @@
+const EBox: React.FC = () => {
+  return (
+    <div>
+      {/* absolote icon */}
+      <span>{/* @todo icon element */}</span>
+
+      {/* date element */}
+      <p>2018 - PRESENT</p>
+
+      {/* title */}
+      <p>
+        {/* title 1 */}
+        <span>WEB DEVELOPER</span>
+        {/* title 2 */}
+        <span>ENVATO</span>
+      </p>
+
+      {/* description */}
+      <p>
+        Lorem ipsum dolor sit amet, consectetur tempor incididunt ut labore
+        adipisicing elit,
+      </p>
+    </div>
+  );
+};
+
+export default EBox;
