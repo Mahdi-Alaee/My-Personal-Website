fa90a5553ee12901873561b76c3f1e33aad0c742
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-03-26 20:36:51 +0330

    portfolio page html structure

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -1,9 +1,30 @@
+import PortfolioBox from "./components/PortfolioBox";
+
 const Portfolio: React.FC = () => {
-    return (
-        <div>
-            Portfolio
+  return (
+    <div className="max-w-6xl mx-auto">
+      {/* title */}
+      <h1 className="uppercase font-black text-center sm:relative">
+        <span className="hidden text-white/5 text-9xl sm:block">works</span>
+        <div
+          className="fixed z-10 text-3xl p-5 w-full left-0 right-0  top-0 m-auto 
+           flex items-center bg-darkBrown sm:absolute sm:bottom-0 sm:w-max sm:text-6xl sm:bg-transparent"
+        >
+          <span>my</span> <span className="text-yellowColor">portfolio</span>
         </div>
-    )
-}
+      </h1>
+
+      {/* content */}
+      <div className="grid grid-cols-3 gap-8">
+        <PortfolioBox />
+        <PortfolioBox />
+        <PortfolioBox />
+        <PortfolioBox />
+        <PortfolioBox />
+        <PortfolioBox />
+      </div>
+    </div>
+  );
+};
 
 export default Portfolio;
---
+++ src/pages/Portfolio/components/PortfolioBox.tsx
@@ -0,0 +1,14 @@
+const PortfolioBox: React.FC = () => {
+    return (
+        <div>
+            {/* image */}
+            <img src="https://tunis-next.netlify.app/assets/img/projects/project-2.jpg" alt="mahdi alaee | portfolio image" />
+            {/* hover element */}
+            <div>
+                some text...
+            </div>
+        </div>
+    )
+}
+
+export default PortfolioBox
