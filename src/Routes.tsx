c70ba8460b2e021a3d1059a5a750a115b94fe85a
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-03-28 20:08:38 +0330

    add Blog route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,10 +3,12 @@ import Index from "./pages/Index";
 import About from "./pages/About/About";
 import Portfolio from "./pages/Portfolio/Portfolio";
 import Contact from "./pages/Contact/Contact";
+import Articles from "./pages/Articles/Articles";
 
 export const routes: RouteObject[] = [
   { path: "", element: <Index /> },
   { path: "/about", element: <About /> },
   { path: "/portfolio", element: <Portfolio /> },
   { path: "/contact", element: <Contact /> },
+  { path: "/blog", element: <Articles /> },
 ];
--- src/pages/Articles/Articles.tsx
+++ src/pages/Articles/Articles.tsx
@@ -2,7 +2,7 @@ import ArticleBox from "../../components/ArticleBox";
 
 const Articles: React.FC = () => {
   return (
-    <div>
+    <div className="pt-20">
       {/* title */}
       <h1 className="uppercase font-black text-center sm:relative">
         <span className="hidden text-white/5 text-9xl sm:block">works</span>
@@ -10,7 +10,7 @@ const Articles: React.FC = () => {
           className="fixed z-10 text-3xl p-5 w-full left-0 right-0  top-0 m-auto 
            flex items-center bg-darkBrown sm:absolute sm:bottom-0 sm:w-max sm:text-6xl sm:bg-transparent"
         >
-          <span>my</span> <span className="text-yellowColor">portfolio</span>
+          <span>my</span> <span className="text-yellowColor">BLOG</span>
         </div>
       </h1>
 
