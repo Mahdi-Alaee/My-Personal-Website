e0da9d64f964bafa30e60b8043e0c2bf53bc7d24
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-03-28 21:02:49 +0330

    add Article page route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,6 +4,7 @@ import About from "./pages/About/About";
 import Portfolio from "./pages/Portfolio/Portfolio";
 import Contact from "./pages/Contact/Contact";
 import Articles from "./pages/Articles/Articles";
+import Article from "./pages/Article/Article";
 
 export const routes: RouteObject[] = [
   { path: "", element: <Index /> },
@@ -11,4 +12,5 @@ export const routes: RouteObject[] = [
   { path: "/portfolio", element: <Portfolio /> },
   { path: "/contact", element: <Contact /> },
   { path: "/blog", element: <Articles /> },
+  { path: "/blog/:articleName", element: <Article /> },
 ];
