e5200ec62e0e65c37c0446d6022de80082a9ca47
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-03-28 21:01:15 +0330

    initialize Article page

--- src/components/ArticleBox.tsx
+++ src/components/ArticleBox.tsx
@@ -4,7 +4,7 @@ const ArticleBox: React.FC = () => {
   return (
     <div className="bg-darkBrown rounded-md overflow-hidden max-w-lg">
       {/* banner */}
-      <Link className="block border-b-[6px] border-yellowColor" to=''>
+      <Link className="block border-b-[6px] border-yellowColor" to='/blog/test-article'>
         <img
           src="https://tunis-next.netlify.app/assets/img/blog/blog-post-1.jpg"
           alt="article banner"
@@ -12,7 +12,7 @@ const ArticleBox: React.FC = () => {
       </Link>
       {/* title */}
       <Link className="block px-6 my-4 text-xl font-bold transition-colors duration-200
-      tracking-wider hover:text-yellowColor" to=''>How to Own Your Audience by Creating an Email List</Link>
+      tracking-wider hover:text-yellowColor" to='/blog/test-article'>How to Own Your Audience by Creating an Email List</Link>
       {/* description */}
       <p className="px-6 pb-6 leading-7">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
