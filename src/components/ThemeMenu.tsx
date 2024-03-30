3d82f02b4b197603b81523a0faa1470178dbc27e
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-03-30 20:55:51 +0330

    ThemeMenu component styles

--- src/components/ThemeMenu.tsx
+++ src/components/ThemeMenu.tsx
@@ -1,22 +1,29 @@
 import { FaMoon } from "react-icons/fa";
 
-const ThemeMenu:React.FC = () => {
-    return (
-        <div>
-            {/* theme */}
-            <section>
-                <span>THEME: </span>
-                <FaMoon />
-            </section>
-            {/* color */}
-            <section>
-                <span>COLOR: </span>
-                <div>
-                    <span className="block w-5 h-5 rounded-full bg-red-500"></span>
-                </div>
-            </section>
+const ThemeMenu: React.FC = () => {
+  return (
+    <div className="absolute top-10 right-20 bg-white text-black z-50 py-4 px-8">
+      {/* theme */}
+      <section className="flex items-center gap-x-2">
+        <span className="">THEME: </span>
+        <span className="p-2 rounded-full bg-darkBrown dark:text-gray-300 cursor-pointer">
+          <FaMoon />
+        </span>
+      </section>
+      {/* color */}
+      <section>
+        <span className="">COLOR: </span>
+        <div className="flex gap-x-2">
+          <span className="cursor-pointer w-7 h-7 rounded-full bg-yellowColor"></span>
+          <span className="cursor-pointer w-7 h-7 rounded-full bg-blue-500"></span>
+          <span className="cursor-pointer w-7 h-7 rounded-full bg-green-500"></span>
+          <span className="cursor-pointer w-7 h-7 rounded-full bg-red-500"></span>
+          <span className="cursor-pointer w-7 h-7 rounded-full bg-orange-500"></span>
+          <span className="cursor-pointer w-7 h-7 rounded-full bg-cyan-600"></span>
         </div>
-    )
-}
+      </section>
+    </div>
+  );
+};
 
 export default ThemeMenu;
