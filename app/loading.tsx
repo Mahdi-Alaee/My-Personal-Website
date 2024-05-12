f96d8a182b4bcdaae6c4cdf7e743721b9eb200c5
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-05-12 17:47:21 +0330

    add loading screen

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import { FaArrowsRotate } from "react-icons/fa6";
+
+export default function loading(){
+    return (
+        <div className="fixed top-0 left-0 bottom-0 right-0 dark:bg-black bg-white flex justify-center items-center">
+            <FaArrowsRotate className="text-yellowColor text-9xl animate-spin" />
+        </div>
+    )
+}
